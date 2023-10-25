5e216cf9b0ce41d5bfd28dbf06a23ed6092a0d22
Tan Nguyen <tannp27@gmail.com>
2023-10-25 08:33:13 +0700

    add nav

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,4 @@
-import { type DefaultTheme } from 'vitepress'
+import {type DefaultTheme} from 'vitepress'
 
 export default ({
     lang: 'en-US',
@@ -8,7 +8,23 @@ export default ({
     lastUpdated: true,
     cleanUrls: true,
 
+    /* prettier-ignore */
+    head: [
+        ['meta', {name: 'theme-color', content: '#5f67ee'}],
+        ['meta', {name: 'og:type', content: 'website'}],
+        ['meta', {name: 'og:locale', content: 'en'}],
+        ['meta', {name: 'og:site_name', content: 'Telegram Git Notifier'}],
+        ['meta', {name: 'og:image', content: 'https://user-images.githubusercontent.com/35853002/277427295-193e523b-4654-4f61-aaf4-5b6d106b67e3.png'}],
+    ],
+
     themeConfig: {
+
+        nav: nav(),
+
+        editLink: {
+            pattern: 'https://github.com/lbiltech/telegram-git-notifier-docs/edit/main/docs/:path',
+            text: 'Edit this page on GitHub'
+        },
 
         socialLinks: [
             {icon: 'github', link: 'https://github.com/lbiltech/telegram-git-notifier'},
@@ -22,3 +38,18 @@ export default ({
         },
     }
 })
+
+function nav(): DefaultTheme.NavItem[] {
+    return [
+        {
+            text: 'Project',
+            link: '/app/index',
+            activeMatch: '/app/'
+        },
+        {
+            text: 'Package',
+            link: '/package/index',
+            activeMatch: '/package/'
+        },
+    ]
+}
